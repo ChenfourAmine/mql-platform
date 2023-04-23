b12af9a529c4fbdf2758608e762e8a7b517edc81
ChenfourAmine <amine.chenfour456@gmail.com>
2023-04-23 18:18:18 +0100

    initial commit

---
+++ src/main/java/org/mql/platform/MqlPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.mql.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqlPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqlPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mql/platform/MqlPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mql.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqlPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
